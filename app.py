b7c3fb68aea29456a07d7960f137d498ecbb9492
ValeryGeorgina714 <valerygeorgina7@gmail.com>
2025-06-28 16:52:00 -0500

    cambios

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ def inicio():
 @app.route('/formulario', methods=['GET', 'POST'])
 def formulario():
     if request.method == 'POST':
-        
         return redirect(url_for('formulario2'))
     return render_template('formulario.html')
 
 @app.route('/formulario2', methods=['GET', 'POST'])
 def formulario2():
     if request.method == 'POST':
-
         return "Formulario enviado correctamente ✅"
     return render_template('formulario2.html')
 
