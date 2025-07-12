7edfe1b2b0407600faa78a23dd2d1ec2e2956268
Romina Ariadna Romero Romero Rivas <romero.rivas.RA@gmail.com>
2025-07-11 22:54:33 -0500

    Modifique los estilos y agrege el login

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+
+#primero se descarga el  flask , si no lo tienes usa esto en tu temrinal -> pip install flask
+#leugo lo ejecutas 
+#el temrinal eses http://127.0.0.1:5000/login
+
+
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'supersecreto'  # Necesario para sesiones
+
+
 
 # Variables globales para almacenar datos de OTM
 otm_parcial = {}   # Guarda temporalmente los datos del Formulario 1
@@ -64,7 +73,56 @@ def formulario2():
 
 @app.route('/resumen')
 def resumen():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
     return render_template('resumen.html', otms=otm_data)
+
+
+# ...login usuarioy contraseña
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Usuario y contraseña de ejemplo
+        if username == 'admin' and password == '1234':
+            session['usuario'] = username
+            return redirect(url_for('resumen'))
+        else:
+            error = 'Usuario o contraseña incorrectos'
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
+
+
+
+#para descargar el excel se instala en su temrinal -> pip install pandas openpyxl  
+
+
+import pandas as pd
+from flask import send_file
+import io
+
+
+
+@app.route('/descargar_excel')
+def descargar_excel():
+    if not otm_data:
+        return "No hay datos para descargar.", 400
+    df = pd.DataFrame(otm_data)
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name='OTMs')
+    output.seek(0)
+    return send_file(output, download_name="OTMs.xlsx", as_attachment=True)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
