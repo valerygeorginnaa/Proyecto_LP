0bd410e0293f4aba44f90d0eab7100f1172f9add
ValeryGeorgina714 <valerygeorgina7@gmail.com>
2025-07-26 10:37:37 -0500

    users

---
+++ usuariosdelsistema.py
@@ -0,0 +1,18 @@
+# usuarios.py
+
+licenciadas = [
+    {'username': 'LICmlopez01', 'password': '123456'},
+    {'username': 'LICjparedes02', 'password': '123456'}
+]
+
+tecnicosingenieros = [
+    {'username': 'INGjgomez01', 'password': '123456'},
+    {'username': 'INGlperez02', 'password': '123456'},
+    {'username': 'TECvpinto19', 'password': '123456'},
+    {'username': 'INGzbarrios04', 'password': '123456'}
+]
+
+secretarias = [
+    {'username': 'SECcrojas01', 'password': '123456'},
+    {'username': 'SECmtorres02', 'password': '123456'}
+]
